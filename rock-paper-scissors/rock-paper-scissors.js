00ec6cd5d748709a39ffef2fcb63348256d64742
Sarvesh <sarveshshahane2004@gmail.com>
2023-10-03 21:51:36 +0530

    change choiceMap to CHOICE_MAP since it is a const

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,7 +1,7 @@
-const choiceMap = new Map();
-choiceMap.set('rock', 1);
-choiceMap.set('paper', 2);
-choiceMap.set('scissors', 3);
+const CHOICE_MAP = new Map();
+CHOICE_MAP.set('rock', 1);
+CHOICE_MAP.set('paper', 2);
+CHOICE_MAP.set('scissors', 3);
 
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
@@ -17,7 +17,7 @@ function getComputerChoice() {
 let playerWinCount = 0;
 
 function playRound(computerSelection, playerSelection) {
-    if (choiceMap.get(computerSelection) % 3 > choiceMap.get(playerSelection.toLowerCase()) % 3) {
+    if (CHOICE_MAP.get(computerSelection) % 3 > CHOICE_MAP.get(playerSelection.toLowerCase()) % 3) {
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
         playerWinCount++;
