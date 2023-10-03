e23e2a9c2d2b84559dee91cd047553d42db588a3
Sarvesh <sarveshshahane2004@gmail.com>
2023-10-03 21:02:08 +0530

    add a hashmap and create a function for  round

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,3 +1,7 @@
+const choiceMap = new Map();
+choiceMap.set('rock', 1);
+choiceMap.set('paper', 2);
+choiceMap.set('scissors', 3);
 function getComputerChoice() {
     let num = floor(Math.random() * 3);
     if (num === 2) {
@@ -7,4 +11,13 @@ function getComputerChoice() {
     } else {
         return 'scissors'; 
     }
-}
+}   
+
+function playRound(computerSelection, playerSelection) {
+    if (choiceMap.get(computerSelection) % 3 > choiceMap.get(playerSelection.toLowerCase()) % 3) {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `You Win!`;
+    }
+}   
+
